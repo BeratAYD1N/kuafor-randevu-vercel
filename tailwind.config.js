36575def680b16f87142498c8931735ac7b93be4
Berat AYDIN <berataydn619@gmail.com>
2025-06-12 12:23:54 +0300

    Initial commit - Elit Kuafor project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,9 @@
 module.exports = {
   darkMode: 'class',
   content: [
-    "./src/**/*.{js,jsx,ts,tsx,mdx}",
-    "./src/app/**/*.{js,jsx,ts,tsx,mdx}",
-    "./src/components/**/*.{js,jsx,ts,tsx,mdx}",
+    './src/**/*.{js,jsx,ts,tsx,mdx}',
+    './src/app/**/*.{js,jsx,ts,tsx,mdx}',
+    './src/components/**/*.{js,jsx,ts,tsx,mdx}',
   ],
   theme: {
     extend: {},
